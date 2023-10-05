9af383fb9f3925f3d63343f9c431c5c8bcca197a
adamrolf <145652267+adamrolf@users.noreply.github.com>
2023-10-05 09:40:39 +0200

    Add files via upload
    adam 2. hodina

---
+++ 2. hodina.cs
@@ -0,0 +1,49 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("ENTER PASSWORD");
+//string password = Console.ReadLine();
+//Console.WriteLine("what is our name?");
+//string name = Console.ReadLine();
+//Console.WriteLine("hello " + name);
+
+//if (password == "1234" )
+//{
+//    Console.WriteLine("you are the chosen one");
+//}
+//else
+//{
+//    Console.WriteLine("wrong password");
+//}
+//for (int i = 0; i < 5; i++)
+//{
+//    Console.WriteLine("hobluj");
+//}
+//int number = 0;
+//while (number < 5)
+//{ 
+//    Console.WriteLine(number);
+//    number = number + 1;
+//{ 
+int otherNumber = 6;
+do
+{ 
+Console.WriteLine(otherNumber);
+    otherNumber = otherNumber + 1;  
+} while (otherNumber < 5);
+
+Console.WriteLine("Enter a number") ;
+string numIterstionstring = Console.ReadLine();
+
+int numIterstions = 0;
+
+bool success = int. TryParse(numIterstionstring, out numIterstions);
+Console.WriteLine("success =" + success);
+for (int i = 0; i < numIterstions; i = i + 1)
+{
+    Console.WriteLine(i);
+}
+if ( success == false)
+do
+{
+    Console.WriteLine("to neni cislo bro delej znova");
+    Console.ReadLine();
+} while (success == false);
