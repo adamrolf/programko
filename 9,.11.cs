a61c6ce4c3360db9472bbcfafc7f16d2f70333f2
adamrolf <145652267+adamrolf@users.noreply.github.com>
2023-11-09 09:44:59 +0100

    Add files via upload

---
+++ 9,.11.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+
+// void PrintArray(int[] array)
+//{
+//    for (int i = 0; i < 3; i++) 
+//    {
+//        Console.WriteLine();
+//    }
+// }
+//int[] numbers = { 1, 2, 3 };
+//PrintArray(numbers);
+int[] coefficients = new int[] { 6, 4, 48, -4, 4, -1 };
+int j = coefficients[0];
+
+{
+    for (int i = 0; i < coefficients.Length; i++)
+    {
+     
+        { 
+        Console.WriteLine(coefficients[i]);
+            Console.WriteLine("x^");
+            Console.WriteLine(coefficients.Length - i, -1);
+    }
+}
+   
+}
