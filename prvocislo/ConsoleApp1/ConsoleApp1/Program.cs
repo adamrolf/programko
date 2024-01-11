8b63b3b3f6e517bf5aabc330e7f67f1b40745fe5
adamrolf <145652267+adamrolf@users.noreply.github.com>
2024-01-11 09:33:04 +0100

    Add files via upload

---
+++ prvocislo/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Zadej číslo:");
+
+        int cislo = int.Parse(Console.ReadLine());
+
+        bool jePrvocislo = JePrvocislo(cislo);
+
+        if (jePrvocislo)
+        {
+            Console.WriteLine($"{cislo} je prvočíslo.");
+        }
+        else
+        {
+            Console.WriteLine($"{cislo} není prvočíslo.");
+        }
+
+        Console.ReadLine();
+    }
+
+
+    static bool JePrvocislo(int number)
+    {
+        if (number <= 1)
+        {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+while (true)
+{
+    Console.WriteLine("Enter number: ");
+    if (int.TryParse(Console.ReadLine(), out int number))
+    {
+        PrintPrimeFact(number);
+    }
+    else
+    {
+        Console.WriteLine("input not valid");
+    }
+    Console.WriteLine("\nPress any key to continue.");
+    Console.ReadKey();
+    Console.Clear();
+
+}
+
+static void PrintPrimeFact(int number)
+{
+    for (int i = 2; i <= number; i++)
+    {
+        while (number % i == 0)
+        {
+            Console.Write($"{i} ");
+            number /= i;
+        }
+    }
+}
